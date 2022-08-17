2bd2201c2d84b53bfaba755d3abbbe0900753e6e
Justin <justin@kauz.ch>
2022-08-17 14:51:48 +0200

    Footer with Github card link insered

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <Main />
+  <div style="min-height: 100vh;">
+    <Main />
+  </div>
+  <Footer />
 </template>
 
 <script setup lang="ts">
 import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <style scoped>
